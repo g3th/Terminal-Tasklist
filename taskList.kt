2a9ba88deacb3186ff7e7afe380e8c034db2fa73
g3th <theinkandpen@yahoo.co.uk>
2023-06-04 04:43:11 +0100

    added date, time and priority

--- taskList.kt
+++ taskList.kt
@@ -1,10 +1,15 @@
 import kotlin.system.exitProcess
+import kotlinx.datetime.*
 
-class taskList {
-    var taskNumber = 1
-    var tasks: MutableMap<Int, String> = mutableMapOf()
+class TaskList {
+    private lateinit var currentTimeDateAndTaskPriority: String
+    private var taskPriority: String = ""
+    private val taskPriorities = listOf("C", "H", "N", "L")
+    private var taskNumber = 1
+    private var tasks: MutableMap<Int, String> = mutableMapOf()
 
     fun add(){
+        priority()
         println("Input a new task (enter a blank line to end):")
         var result = ""
         var input: String
@@ -16,16 +21,61 @@ class taskList {
             } else if (input.all{ it.isWhitespace() }) {
                 break
             } else if (taskNumber < 10) {
-                result += "  $input\n "
+                result += "   $input\n"
             } else if (taskNumber >= 10) {
-                result += " $input\n "
+                result += "   $input\n"
             }
         }
         if(result.isNotEmpty()) {
-            tasks[taskNumber] = result.trimEnd()
+            if (taskNumber < 10) {
+                tasks[taskNumber] = "  ${currentTimeDateAndTaskPriority}\n${result.trimEnd()}"
+            } else {
+                tasks[taskNumber] = " ${currentTimeDateAndTaskPriority}\n${result.trimEnd()}"
+            }
             taskNumber++
         }
+        taskPriority = ""
+    }
 
+    private fun timeDate(){
+        topOfLoop@ while(true) {
+            println("Input the date (yyyy-mm-dd):")
+            val date = readln().split("-")
+            try {
+                LocalDate(date[0].toInt(), date[1].toInt(), date[2].toInt())
+                while(true) {
+                    println("Input the time (hh:mm):")
+                    val time = readln().split(":")
+                    try {
+                        LocalDateTime(date[0].toInt(), date[1].toInt(),
+                            date[2].toInt(),time[0].toInt(),time[1].toInt())
+                        //LocalTime(time[0].toInt(),time[1].toInt())
+                        currentTimeDateAndTaskPriority = "${LocalDateTime(date[0].toInt(), date[1].toInt(),
+                                                         date[2].toInt(),time[0].toInt(),time[1].toInt())
+                                                        .toString().replace("T"," ")} $taskPriority"
+                        break@topOfLoop
+                    } catch (e:Exception) {
+                        println("The input time is invalid")
+                    }
+                }
+            } catch (e: Exception) {
+                println("The input date is invalid")
+            }
+        }
+    }
+
+    private fun priority(){
+        while (true){
+            println("Input the task priority (C, H, N, L):")
+            val input = readln()
+            when {
+                taskPriorities.any{ it.equals(input,true) } -> {
+                    taskPriority += input
+                    timeDate()
+                    break
+                }
+            }
+        }
     }
 
     fun printList(){
@@ -42,7 +92,7 @@ class taskList {
 }
 
 fun main(){
-    val listOfTasks = taskList()
+    val listOfTasks = TaskList()
     while(true) {
         println("Input an action (add, print, end):")
         val userInput = readln()
