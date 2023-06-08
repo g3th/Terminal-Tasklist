d0af2114e8cd7d512c57e50e8111a69be4b7d10e
g3th <theinkandpen@yahoo.co.uk>
2023-06-09 00:47:26 +0100

    added printout module, updated README

---
+++ printout.kt
@@ -0,0 +1,52 @@
+package Terminal.Tasklist
+
+data class tasks (val tasksTDP: List<String>, val task: List<String>)
+
+class PrintOut {
+
+    val header_top = """+----+------------+-------+---+---+--------------------------------------------+
+| N  |    Date    | Time  | P | D |                   Task                     |
++----+------------+-------+---+---+--------------------------------------------+
+"""
+    val header_bottom = "\n+----+------------+-------+---+---+--------------------------------------------+"
+
+    fun taskLayout(input: String): String {
+        var printoutInput = ""
+        val strList = input.split("\n")
+        var chunkedList = listOf<String>()
+        for (i in strList) {
+            chunkedList += i.chunked(44)
+        }
+        for(i in chunkedList.indices){
+            if (chunkedList[i].length < 44) {
+                if (i == 0){
+                    printoutInput += "${chunkedList[i]} ${" ".repeat(43 - chunkedList[i].length)}|"
+
+                } else {
+                    printoutInput += "\n|    |            |       |   |   |${chunkedList[i]} ${" ".repeat(43 - chunkedList[i].length)}|"
+                }
+            } else {
+                if (i == 0){
+                    printoutInput += "${chunkedList[i]}|"
+                } else {
+                    printoutInput += "\n|    |            |       |   |   |${chunkedList[i]}|"
+                }
+            }
+        }
+        return printoutInput
+    }
+
+    fun printTasks(tasksDateTimePriority: MutableList<String>, tasks:MutableList<String>){
+        val tasklist = mutableListOf(tasks(tasksDateTimePriority, tasks))
+        var counter = 1
+        print(header_top)
+        for ((a,b) in tasklist) {
+            for (c in a.indices) {
+                print("| $counter  | ${a[c]} |${b[c]}")
+                println(header_bottom)
+                counter++
+            }
+        }
+        println()
+    }
+}
--- tasklist.kt
+++ tasklist.kt
@@ -1,3 +1,5 @@
+package Terminal.Tasklist
+
 import kotlin.system.exitProcess
 import kotlinx.datetime.*
 
@@ -11,11 +13,13 @@ class TaskList {
     private var result: String = ""
     private var taskPriority: String = ""
     private val taskPriorities = listOf("C", "H", "N", "L")
+    var tasksTimeDatePriority = mutableListOf<String>()
     var tasks = mutableListOf<String>()
     private val currentTime = Clock.System.now()
-        .toLocalDateTime(TimeZone.of("UTC+2"))
-        .date /* Time zone is probably wrong.
-              */
+        .toLocalDateTime(TimeZone.of("UTC+1"))
+        .date
+
+
     fun addTask() {
         priority()
         date()
@@ -23,21 +27,21 @@ class TaskList {
         task()
     }
 
-    fun deleteTask(){
-        printTasks()
+    fun deleteTask() {
         while (true) {
-            println("Input the task number (1-${tasks.size}):")
+            println("Input the task number (1-${tasksTimeDatePriority.size}):")
             try {
                 val taskIndex = readln().toInt()
                 when {
-                    taskIndex < 1 || taskIndex > tasks.size -> println("Invalid task number")
+                    taskIndex < 1 || taskIndex > tasksTimeDatePriority.size -> println("Invalid task number")
                     else -> {
-                        tasks.removeAt(taskIndex - 1)
+                        tasksTimeDatePriority.removeAt(taskIndex - 1)
+                        tasks.removeAt(taskIndex -1)
                         println("The task is deleted")
                         break
                     }
                 }
-            }catch(e: NumberFormatException){
+            } catch (e: NumberFormatException) {
                 println("Invalid task number")
             }
         }
@@ -46,13 +50,12 @@ class TaskList {
     fun editTask() {
         val currentTaskPriority = taskPriority
         val currentTaskDate = givenTaskDate
-        printTasks()
         topOfLoop@ while (true) {
-            println("Input the task number (1-${tasks.size}):")
+            println("Input the task number (1-${tasksTimeDatePriority.size}):")
             try {
                 val taskIndex = readln().toInt()
                 when {
-                    taskIndex < 1 || taskIndex > tasks.size -> println("Invalid task number")
+                    taskIndex < 1 || taskIndex > tasksTimeDatePriority.size -> println("Invalid task number")
                     else -> {
                         while (true) {
                             println("Input a field to edit (priority, date, time, task):")
@@ -60,9 +63,9 @@ class TaskList {
                             when {
                                 input.equals("priority", true) -> {
                                     val newPriority = priority()
-                                    val assignNewPriority = tasks[taskIndex - 1]
+                                    val assignNewPriority = tasksTimeDatePriority[taskIndex - 1]
                                         .replaceFirst(" $currentTaskPriority ", " $newPriority ")
-                                    tasks[taskIndex - 1] = assignNewPriority
+                                    tasksTimeDatePriority[taskIndex - 1] = assignNewPriority
                                     println("The task is changed")
                                     break@topOfLoop
                                 }
@@ -70,10 +73,10 @@ class TaskList {
                                 input.equals("date", true) -> {
                                     val currentTag = tag
                                     date()
-                                    val assignNewDate = tasks[taskIndex - 1]
+                                    val assignNewDate = tasksTimeDatePriority[taskIndex - 1]
                                         .replaceFirst(currentTaskDate.toString(), givenTaskDate.toString())
                                         .replaceFirst(currentTag, tag)
-                                    tasks[taskIndex - 1] = assignNewDate
+                                    tasksTimeDatePriority[taskIndex - 1] = assignNewDate
                                     println("The task is changed")
                                     break@topOfLoop
                                 }
@@ -81,63 +84,68 @@ class TaskList {
                                 input.equals("time", true) -> {
                                     val currentTime = givenTaskDateAndTime.split("T")[1]
                                     time()
-                                    val assignNewTime = tasks[taskIndex - 1]
+                                    val assignNewTime = tasksTimeDatePriority[taskIndex - 1]
                                         .replaceFirst(currentTime, givenTaskDateAndTime.split("T")[1])
-                                    tasks[taskIndex - 1] = assignNewTime
+                                    tasksTimeDatePriority[taskIndex - 1] = assignNewTime
                                     println("The task is changed")
                                     break@topOfLoop
                                 }
 
                                 input.equals("task", true) -> {
                                     taskIsBeingEdited = true
-                                    val currentTask = tasks[taskIndex - 1].split("\n").drop(1)
+                                    val currentTask = tasks[taskIndex - 1]
                                     val newTask = task()
                                     val assignNewTask = tasks[taskIndex - 1]
-                                        .replace(currentTask.joinToString("\n"), newTask)
+                                        .replace(currentTask, newTask)
                                     tasks[taskIndex - 1] = assignNewTask
                                     println("The task is changed")
                                     taskIsBeingEdited = false
                                     break@topOfLoop
                                 }
-                                else -> println("Invalid field")
                             }
                         }
                     }
                 }
-            }catch(e: NumberFormatException){
+            } catch (e: NumberFormatException) {
                 println("Invalid task number")
             }
         }
     }
 
     private fun task(): String {
-        result=""
+        var counter = 0
+        var tempAppend = ""
         var input: String
         println("Input a new task (enter a blank line to end):")
         while (true) {
-            input = readln().trim()
-            if (input.all { it.isWhitespace() } && result.isEmpty()) {
+            input = readln()
+            if (input.all { it.isWhitespace() } && tempAppend.isEmpty()) {
                 println("The task is blank")
                 break
             } else if (input.all { it.isWhitespace() }) {
                 break
-            } else if (tasks.size < 10) {
-                result += "   $input\n"
             } else {
-                result += "   $input\n"
+                if (counter == 0) {
+                    tempAppend += input
+                } else {
+                    tempAppend += "\n$input"
+                }
             }
+            counter++
         }
+        result = PrintOut().taskLayout(tempAppend)
         if (result.isNotEmpty() && !taskIsBeingEdited) {
-            if (tasks.size < 9) {
-                tasks.add("  ${currentTimeDateAndTaskPriority}\n${result.trimEnd()}")
-            } else {
-                tasks.add(" ${currentTimeDateAndTaskPriority}\n${result.trimEnd()}")
-            }
+            tasksTimeDatePriority.add(
+                "${givenTaskDate} | ${givenTaskDateAndTime.split("T")[1]} | " +
+                        "${taskPriority} | $tag"
+            )
+            tasks.add(result)
         }
         return result
     }
-    private fun date(){
-        while(true) {
+
+    private fun date() {
+        while (true) {
             println("Input the date (yyyy-mm-dd):")
             inputtedDate = readln().split("-")
             try {
@@ -151,63 +159,65 @@ class TaskList {
         }
     }
 
-    private fun time(){
-        while(true) {
+    private fun time() {
+        while (true) {
             println("Input the time (hh:mm):")
             val inputtedTime = readln().split(":")
             try {
-                givenTaskDateAndTime = LocalDateTime(inputtedDate[0].toInt(), inputtedDate[1].toInt(),
-                    inputtedDate[2].toInt(),inputtedTime[0].toInt(),inputtedTime[1].toInt()).toString()
+                givenTaskDateAndTime = LocalDateTime(
+                    inputtedDate[0].toInt(), inputtedDate[1].toInt(),
+                    inputtedDate[2].toInt(), inputtedTime[0].toInt(), inputtedTime[1].toInt()
+                ).toString()
 
-                /* Rewrite to include LocalTime
-                   (update datetime to 0.4.0 instead of 0.3.1)
-                */
+                /* This works with datetime 0.4.0:
+                   'LocalTime(time[0].toInt(),time[1].toInt())'
+                   But Hyperskill have the outdated 0.3.1
+                  */
 
-                currentTimeDateAndTaskPriority = "${givenTaskDateAndTime
-                    .replace("T"," ")} $taskPriority $tag"
+                currentTimeDateAndTaskPriority = "${
+                    givenTaskDateAndTime
+                        .replace("T", " ")
+                } $taskPriority $tag"
                 break
-            } catch (e:Exception) {
+            } catch (e: Exception) {
                 println("The input time is invalid")
             }
         }
     }
 
-    private fun appendDueTag(date: LocalDate): String{
+    private fun appendDueTag(date: LocalDate): String {
         lateinit var dueTag: String
         when {
-            date.daysUntil(currentTime) > 0 -> dueTag = "O"
-            date.daysUntil(currentTime) < 0 -> dueTag = "I"
-            date.daysUntil(currentTime) == 0 -> dueTag = "T"
+            date.daysUntil(currentTime) > 0 -> dueTag = "\u001B[101m \u001B[0m" //101
+            date.daysUntil(currentTime) < 0 -> dueTag = "\u001B[102m \u001B[0m" //102
+            date.daysUntil(currentTime) == 0 -> dueTag = "\u001B[103m \u001B[0m" //103
         }
         return dueTag
     }
 
-    private fun priority(): String{
-        while (true){
+    private fun priority(): String {
+        while (true) {
             println("Input the task priority (C, H, N, L):")
             val input = readln()
             when {
-                taskPriorities.any{ it.equals(input,true) } -> {
-                    taskPriority = input
+                taskPriorities.any { it.equals(input, true) } -> {
+                    when {
+                        input.equals("C", ignoreCase = true) -> taskPriority = "\u001B[101m \u001B[0m"
+                        input.equals("H", ignoreCase = true) -> taskPriority = "\u001B[103m \u001B[0m"
+                        input.equals("N", ignoreCase = true) -> taskPriority = "\u001B[102m \u001B[0m"
+                        input.equals("L", ignoreCase = true) -> taskPriority = "\u001B[104m \u001B[0m"
+                    }
                     break
                 }
             }
         }
+        result = ""
         return taskPriority
-    }
-
-    fun printTasks(){
-        var counter = 1
-        for (i in tasks) {
-            println()
-            print("$counter${i}\n")
-            counter++
-        }
-        println()
     }
 }
 
 fun main() {
+    val print = PrintOut()
     val actionList = listOf("print", "edit", "delete")
     val listOfTasks = TaskList()
     while (true) {
@@ -218,9 +228,15 @@ fun main() {
                 println("No tasks have been input")
             }
             userInput.equals("add", true) -> listOfTasks.addTask()
-            userInput.equals("print", true) -> listOfTasks.printTasks()
-            userInput.equals("delete", true) -> listOfTasks.deleteTask()
-            userInput.equals("edit", true) -> listOfTasks.editTask()
+            userInput.equals("print", true) -> print.printTasks(listOfTasks.tasksTimeDatePriority, listOfTasks.tasks)
+            userInput.equals("delete", true) -> {
+                print.printTasks(listOfTasks.tasksTimeDatePriority, listOfTasks.tasks)
+                listOfTasks.deleteTask()
+            }
+            userInput.equals("edit", true) -> {
+                print.printTasks(listOfTasks.tasksTimeDatePriority, listOfTasks.tasks)
+                listOfTasks.editTask()
+            }
             userInput.equals("end", true) -> {
                 println("Tasklist exiting!")
                 exitProcess(0)
